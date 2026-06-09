550b473fa0735ebb2453f0f29a8334cc3a26c238
kshams19777-hue <kshams19777@gmail.com>
2026-06-09 07:25:49 +0000

    completed tutorial 1

---
+++ tutorial/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("mohamed", 20, 3.7);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
